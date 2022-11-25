da40c2d0139090f30aaad15b35b4ef16e9e6c455
Elias <ELias@root.com>
2022-11-25 21:27:30 +0800

    Downloading xlsx

--- client/src/views/ReportBySchool.vue
+++ client/src/views/ReportBySchool.vue
@@ -1,196 +1,177 @@
 <script setup>
-  import { storeToRefs } from "pinia";
-  import { useLessonsStore } from "../stores/lessons";
-  import { computed, onBeforeMount, ref } from "vue";
-  import xlsx from "json-as-xlsx"
-  import { groupByStudent } from "../util/group";
+import { storeToRefs } from "pinia";
+import { useLessonsStore } from "../stores/lessons";
+import { computed, onBeforeMount, ref } from "vue";
+import xlsx from "json-as-xlsx"
+import { groupByStudent } from "../util/group";
 
-  const lessonsStore = useLessonsStore();
-  const { reports } = storeToRefs(lessonsStore);
-  const { schools } = storeToRefs(lessonsStore);
+import ReportRow from "@/components/ReportRow.vue";
+import _ from "lodash";
+import axios from 'axios';
 
-  const reportsStored = computed(() => {
+const lessonsStore = useLessonsStore();
+const { reports } = storeToRefs(lessonsStore);
+const { schools } = storeToRefs(lessonsStore);
+let selectedSchool = ref("");
+
+function padStr(i) {
+    return (i < 10) ? "0" + i : "" + i;
+}
+function resMark(etud_id, lessons_name, date){
+    return groupedStudents.value[etud_id].lessons[lessons_name][date] || ""
+
+}
+const reportsStored = computed(() => {
   return _(reports.value)
     .orderBy((x) => x[sortFiled.value])
     .value();
-  });
+});
 
-  const reportByStudent = computed(() => {
-    return reports.value
-      ? groupByStudent(reports.value["school-journal"] || [])
-      : [];
-  });
+const reportByStudent = computed(() => {
+  return reports.value
+    ? groupByStudent(reports.value["school-journal"] || [])
+    : [];
+});
 
-  let sortFiled = ref("name");
+let sortFiled = ref("name");
 
-  onBeforeMount(() => {
-    // lessonsStore.fetchReports("1");
-    lessonsStore.fetchSchools();
-  });
-</script>
+onBeforeMount(() => {
+  // lessonsStore.fetchReports("1");
+  lessonsStore.fetchSchools();
+});
 
-<script>
-  import { useLessonsStore } from "../stores/lessons";
-  import ReportRow from "@/components/ReportRow.vue";
-  import _ from "lodash";
-  function proceeded(etuds,e0,e1){
-    for (var i = 0; i < etuds.length; i++){
-      if (etuds[i][0] == e0 && etuds[i][1] == e1){
-        console.log(etuds[i][0] == e0 && etuds[i][1] == e1)
-        return true;
-      }        
-      }
-      console.log(false)
-    return false;
-  }
-  function resEtudName(data,etud,j){
-    for (var i = 0; i < data.length; i++){
-      if (etud[j][0] == data[i]['students'] ){
-        return data[i]['students_name'];
-      }
-    }
-    return '';
-  }
-  function resDate(data,header,etuds,x,y){
-    let date = header[y + 2]
-    let etud = etuds[x][0]
-    let suj = etuds[x][1]
-    for (let i = 0; i < data.length; i++){
-      if (data[i]['date'] == date && data[i]['students'] == etud && data[i]['lessons_name'] == suj)
-        return i;
-    }
-    return -1;
-  }
-  async function fetchReports(subjectId){
-    let url1 = `/api/journals/${subjectId}/school/`;
-    let data1 = new Object();
-    fetch(url1).then(response => 
-        response.json().then(data => ({
-            data: data,
-            status: response.status
-        })
-    ).then(res => {
-      data1 = res.data['school-journal']
-      console.log(res.status, data1)
-      let header = new Array(); // { label: "User", value: "user" }, // Top level data
-      let headeralias = new Array();
-      let headerfields = new Array();
-      header.push('ФИО'); headeralias.push('user'); headerfields.push('students_name');
-      header.push('Предмет'); headeralias.push('suj'); headerfields.push('lessons_name');
-      for (var i = 0; i < data1.length ; i++){
-        if (!header.includes(data1[i]['date'])){
-          header.push(data1[i]['date']);
-          headeralias.push('d' + i.toString());
-          headerfields.push('mark');
+const journal = computed(() => {
+  return reports.value["school-journal"] || [];
+})
+
+const lesDates = computed(() => {
+  let r = _(journal.value)
+    .map(x => x.date)
+    .uniq()
+    .map(x => new Date(x))
+    .orderBy()
+    .map(x => padStr(x.getFullYear()) + '-' +
+              padStr(1 + x.getMonth()) +'-' +
+              padStr(x.getDate()))
+    .value();
+
+    return r;
+})
+
+const groupedStudents = computed(() => {
+  let data = {};
+  for(let x of journal.value) {
+      console.log(x.lessons_name);
+      if (!data[x.students]) {
+        data[x.students] = {
+          lessons: {},
+          students_name: x.students_name,
         }
       }
-      //forming a header
-      var jsonData = "[ { \"sheet\": \"Etuds\", \n \"columns\": [\n"; 
-      for (var i = 0; i < header.length; i++){        
-        jsonData = jsonData + '{ \"label\": \"'+ header[i] +'\", \"value\": \"' + headeralias[i] + '\"},'
-      }
-      //forming etud list
-      let etuds = new Array();
-      let etudsrecs = new Array();
-      for (var i = 0; i < data1.length; i++){
-        if (!proceeded(etuds,data1[i]['students'], data1[i]['lessons_name'])){
-          etuds.push([data1[i]['students'],data1[i]['lessons_name']]);
-          etudsrecs.push([]);
-          for (var j = 0; j < data1.length; j++){
-            if (data1[j]['students'] == etuds[etuds.length - 1][0] && data1[j]['lessons_name'] == etuds[etuds.length - 1][1]){              
-              etudsrecs[etuds.length - 1].push(j)
-            }
-          }
-        }
-      }
-      //forming content
-      jsonData = jsonData.slice(0,jsonData.lastIndexOf(',')) + '],\n\"content\": [';
-      for (var i = 0; i < etuds.length; i++){
-        var name = resEtudName(data1,etuds,i);
-        jsonData = jsonData + '{ \"' + headeralias[0] + '\" : \"' + name + '\",'
-        jsonData = jsonData + '  \"' + headeralias[1] + '\" : \"' + etuds[i][1]+ '\",'
-        for (var j = 0; j < etudsrecs[i].length; j++){ 
-          jsonData = jsonData + ' \"' + headeralias[j + 2] + '\" : \"'
-          //система разрешения даты и оценки
-          let h = resDate(data1,header,etuds,i,j);
-          if (h != -1){
-            jsonData = jsonData + data1[h]['mark'] + '\" ';
-          }
-          else {
-            jsonData = jsonData + '\"';
-          }
-          if (j < etudsrecs[i].length - 1) jsonData = jsonData + ',';
-        }
-        jsonData = jsonData + '},'
-      }
-      jsonData = jsonData.slice(0,jsonData.lastIndexOf(',')) + ']}]';
-      console.log(jsonData);
-      let ectitle = document.getElementById("selececol").options[document.getElementById("selececol").selectedIndex].text;
-      let settings = {
-        fileName: "Отчёт для " + ectitle, // Name of the resulting spreadsheet
-        extraLength: 3, // A bigger number means that columns will be wider
-        writeMode: 'writeFile', // The available parameters are 'WriteFile' and 'write'. This setting is optional. Useful in such cases https://docs.sheetjs.com/docs/solutions/output#example-remote-file
-        writeOptions: {}, // Style options from https://github.com/SheetJS/sheetjs#writing-options
-        RTL: false, // Display the columns from right-to-left (the default value is false)
-    }
-    let data = JSON.parse(jsonData);
-    xlsx(data, settings)
-    }));
-  }
 
-  function downloadXLSX(){
-    let ec = document.getElementById("selececol").options[document.getElementById("selececol").selectedIndex].value.toString(10);
-    
-    fetchReports(ec);
-  }
-  
-  export default {
-    data() {
-      return {
-        selectedSchool: "",
-      };
-    },
-    methods: {
-      onSelectClick(id) {
-        console.log(id);
-        console.log(this.reportsStored);
-        console.log(this.reportsStored[0]);
-        console.log(this.reportByStudent);
-        this.lessonsStore.fetchReports(id);
-      },
-      onClick() {
-      
-      },
-    },
+      if (!data[x.students].lessons[x.lessons_name]) {
+        data[x.students].lessons[x.lessons_name] = {}
+      }
+
+      data[x.students].lessons[x.lessons_name][x.date] = x.mark;
   };
+  console.log(data)
+  return data;
+})
+
+async function fetchReports(subjectId) {
+
+  let response = await axios.get(`/api/journals/${subjectId}/school/`);
+  let res = response.data;
+  let data1 = response['school-journal'];
+  journal.value = data1;
+  let header = new Array(); // { label: "User", value: "user" },
+  let headeralias = new Array();
+  header.push('ФИО'); headeralias.push('user');
+  header.push('Предмет'); headeralias.push('suj');
+  for (var i = 0; i < lesDates.value.length; i++) {
+    header.push(lesDates.value[i]);
+    headeralias.push('d' + i.toString());
+  }
+  console.log(header,headeralias)
+  //forming a header
+  let data = {sheet: "Etuds", columns: [],content: []};
+  for (var i = 0; i < header.length; i++) {
+    data.columns.push(
+      {'label': header[i], value: headeralias[i]}
+    )
+  }
+  //forming content
+  for (let key0 in groupedStudents.value) {
+    for (let key1 in groupedStudents.value[key0].lessons) {
+      let currlist = { [headeralias[0]]: groupedStudents.value[key0].students_name, [headeralias[1]]: key1}
+      for (let i = 2; i < header.length; i++){
+        currlist[headeralias[i]] = resMark(key0,key1,header[i]) //etud_id, lessons_name, date
+      }
+      data.content.push(currlist)
+    }
+  }
+  console.log(JSON.stringify(data))
+  let ectitle = document.getElementById("selececol").options[document.getElementById("selececol").selectedIndex].text;
+  let settings = {
+    fileName: "Отчёт для " + ectitle, // Name of the resulting spreadsheet
+    extraLength: 3, // A bigger number means that columns will be wider
+    writeMode: 'writeFile', // The available parameters are 'WriteFile' and 'write'. This setting is optional. Useful in such cases https://docs.sheetjs.com/docs/solutions/output#example-remote-file
+    writeOptions: {}, // Style options from https://github.com/SheetJS/sheetjs#writing-options
+    RTL: false, // Display the columns from right-to-left (the default value is false)
+  }
+  xlsx(JSON.parse('['+JSON.stringify(data)+']'), settings)
+}
+
+function downloadXLSX() {
+  let ec = document.getElementById("selececol")
+    .options[document.getElementById("selececol").selectedIndex]
+    .value.toString(10);
+  fetchReports(ec);
+}
+
+function onSelectClick(id) {
+  console.log(id);
+  console.log(this.reportsStored);
+  console.log(this.reportsStored[0]);
+  console.log(this.reportByStudent);
+  this.lessonsStore.fetchReports(id);
+
+  
+}
+
 </script>
 
 <template>
   <h2>Отчёт по школам</h2>
-
-  <select v-model="selectedSchool" id = 'selececol'>
+  <select v-model="selectedSchool" id='selececol'>
     <option disabled value="">Выберите один из вариантов</option>
     <option v-for="s in schools" :value="s.id">
       {{ s.title }}
     </option>
   </select>
-  <button class = "btn btn-info ms-2" @click="onSelectClick(selectedSchool)">Показать</button>
-  <button class = "btn btn-info ms-2" @click="onClick(downloadXLSX())">Скачать отчёт</button>
-  <div id = "jour">
-    <div id = 'head' class="journal_header">
-      <span id = 'head0'>ФИО</span>
-      <span id = 'head1'>Предмет</span>
-      <span id = 'head3'>Оценки</span>
-    </div>
-      <ReportRow
-        v-for="(r, index) in reportByStudent"
-        :number = "index"
-        :students_name = "r.students_name"
-        :lessons_name = "r.lessons_name"
-        :date = "r.date"
-        :mark = "r.marks.join(' ')"
-      />
-    </div>
+  <button class="btn btn-info ms-2" @click="onSelectClick(selectedSchool)">Показать</button>
+  <button class="btn btn-info ms-2" @click="downloadXLSX">Скачать отчёт</button>
+
+  <table class="table table-bordered" >
+    <thead>
+      <th>ФИО</th>
+      <th>Предмет</th>
+      <th v-for="d in lesDates">{{d}}</th>
+    </thead>
+    <tbody>
+      <template v-for="(student, sID) in groupedStudents">
+        <tr v-for="(dates, lessonName) in student.lessons">
+          <td>{{student.students_name}}</td>
+          <td>{{lessonName}}</td>
+          <th v-for="d in lesDates">
+            {{ dates[d] }}
+          </th>
+        </tr>
+      </template>
+    </tbody>
+  </table>
+
 </template>
 
 <style scoped>
@@ -200,14 +181,14 @@
   margin-top: 20px;
 }
 
-.journal_header > span {
+.journal_header>span {
   width: calc(100% / 3);
   border: 1px solid black;
   border-right: none;
   padding: 5px;
 }
 
-.journal_header > span:last-child {
+.journal_header>span:last-child {
   border-right: 1px solid black;
 }
 </style>
